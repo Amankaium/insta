3659b4c985271edb5b05ad148701ad293eecdfe5
Amankaium <kaium@list.ru>
2020-09-22 20:21:34 +0600

    modified profile EP, added subs and dubs list

--- core/models.py
+++ core/models.py
@@ -51,6 +51,8 @@ class Profile(BaseModel):
         verbose_name="Подписка"
     )
 
+    
+
 
 
     
--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 
 urlpatterns = [
-    path("<int:pk>/", ProfileRetrieveView.as_view(), name="profile"),
+    # path("<username>/", ProfileRetrieveView.as_view(), name="profile"),
+    path("<username>/", UserRetrieveView.as_view(), name="get-user"),
+    path("<username>/following/", SubscribingListView.as_view(), name='subscribing-list'),
+    path("<username>/followers/", SubscribersListView.as_view())
 ]
